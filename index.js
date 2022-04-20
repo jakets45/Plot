3e68d6a899595c83e63f45c51146d404b2c39e97
Jakob Visic <jakob.visic@gmail.com>
2022-04-20 11:22:54 -0400

    make it better

--- index.js
+++ index.js
@@ -21,66 +21,97 @@ const axios = require('axios');
 /* SETUP EXPRESS */
 const express = require ('express')   // express framework 
 const cors = require ('cors')         // Cross Origin Resource Sharing
-const bodyParser = require('body-parser') // middleware to parse JSON data that is sent from the frontend.
+const bodyParser = require('body-parser'); // middleware to parse JSON data that is sent from the frontend.
+const { response } = require('express');
 const app = express(); // enable express
 app.use( cors() ); // make express attach CORS headers to responses
-app.use( express.json() ); // add json capabilities to our express app 
+app.use(express.json({limit: '50mb', extended: true})); // add json capabilities to our express app 
 
 /* Serve up static assets, i.e. the Frontend of the site. */
 app.use( '/', express.static('public')) 
 
+// app.use(express.json({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
   
 /** listen for users' searches from the frontend */
+// User enters into search, node listens to search and then relays to api
+
 app.get('/search', async (req,res) => { 
-    /** relay search filters to the openfarm API */
+    /** relay search filters to the pokemon API */
+    // send  the search to the endpoint,  (node listens for the request and then sends it to a third part  (from front-end search bar, to node js, then to the pokemon api))
     axios.get(
-        'https://openfarm.cc/api/v1/crops/', 
+        'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20000', 
         {params: {filter: req.query.filter}}
     )
     .then( results => { 
-        // check if the crops have images or not before sending them.
-        let cropsWithImage = results.data.data.filter(
-            crop=>crop.attributes.main_image_path.includes('s3.amazon')
+      
+        let pokemonWithUrl = results.data.results.filter(
+            pokemon => pokemon.url.includes('pokeapi')
         )
-        if( cropsWithImage.length) return res.send(cropsWithImage) 
-        res.send("No Results")  
         
+        if (pokemonWithUrl.length) return res.send(pokemonWithUrl)
+        res.send("No results")
+
+        // for (pokemon of response.data) {
+        //     renderPokemon(pokemon);
+        // }
+    
     })
     .catch( err=> res.send("Search Error") )
+    // axios.get(pokemonWithUrl.URL).then( resp => {  let pokemonURL = response.data;  })
 })
+
+
+const renderPokemon = (pokemon) => {
+    console.log("running");
+    axios.get(pokemon.url)
+    .then(function (response) {
+      
+      // console.log(pokemon.url);
+      // an array of character objects will be stored in this variable. 
+        let pokemonURL = response.data;
+        console.log(pokemonURL);
+        document.getElementById("pokeImage").src = pokemonURL.sprites.front_default.toString();
+
+    })
+}
+
 
 // Connect to MongoDB
 // See also https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/
 MongoClient.connect(MONGODB_URI, { useUnifiedTopology: true })
 .then(client =>{ 
-    const myCollection = client.db("plot").collection("plots")
+    const myCollection = client.db("team").collection("teams")
 
-/** fetch a plot (list of crops) for a given farmer. */
-app.get('/plot', (req,res) => {   
+/** fetch a plot (list of crops) for a given trainer. */
+app.get('/team', (req,res) => {   
     myCollection.findOne(
-        { farmer: req.query.farmer  }, 
-        (error, plot)=>{
+        { trainer: req.query.trainer  }, 
+        (error, team)=>{
             /** If there is no result send a blank default plot. */
-            if (error || plot == null) {
+            if (error || team == null) {
                 return res.send({
-                    farmer:req.query.farmer, 
-                    crops: []
+                    trainer:req.query.trainer, 
+                    pokemons: []
                 })
             } 
             /** send the full data */
-            res.send( plot )
+            res.send( team )
         }
     ) 
 })
 
-/** Add/update a plot for a given farmer. 
+
+
+/** Add/update a plot for a given trainer. 
  * See also: https://www.mongodb.com/docs/drivers/node/current/usage-examples/updateOne/
  * Read more about update operators: 
  * https://www.mongodb.com/docs/manual/reference/operator/update/#update-operators  */
-app.post( '/plot', bodyParser.json(), (req,res) => { 
+app.post( '/team', bodyParser.json(), (req,res) => { 
     myCollection.updateOne(
-        {farmer: req.body.farmer}, 
-        {$set: { crops : req.body.crops } },  
+        {trainer: req.body.trainer}, 
+        {$set: { pokemons : req.body.pokemons } },  
         {upsert: true},  /** upsert = create if it doesnt exist. */
         (error) => { 
             if (error)  return res.send('Error') 
@@ -95,7 +126,7 @@ app.post( '/plot', bodyParser.json(), (req,res) => {
 
 
 /** Tell Express to start listening. */
-const PORT = process.env.PORT || 5000  
+const PORT = process.env.PORT || 6001 
 app.listen(PORT, () => {
   console.log("We are live on port "+PORT )
 })
--- public/script.js
+++ public/script.js
@@ -1,73 +1,200 @@
-const app = Vue.createApp({ 
-    data(){
-        return {  
+// const { default: axios } = require("axios");
+
+const app = Vue.createApp({
+    data() {
+        return {
             myNameIs: null,
             searchFilter: null,
             results: null,      /** array: search results (crops) */
             notice: null,       /** string: for any warnings/errors */
-            plot: {
-                farmer: null,
-                crops: []
-            }          /** array: crops owned by farmer */
+            team: {
+                trainer: null,
+                pokemons: []
+            },          /** array: crops owned by team */
+            moves: []
         }
     },
-    methods:{     
+    methods: {
         /**  process the hello form to get the user's name  
-         *  Ask NodeJS / MongoDB to find the farmer's plot
-         * i.e. which crops the farmer already owns */
-        hello(){  
-            axios.get('/plot', {params: {farmer: this.myNameIs}} )
-                .then(response =>  this.plot = response.data )
-                .catch(error =>  this.showNotice("Error." ))
+         *  Ask NodeJS / MongoDB to find the team's plot
+         * i.e. which crops the team already owns */
+        hello() {
+            axios.get('/team', { params: { trainer: this.myNameIs } })
+                .then(response => this.team = response.data)
+                .catch(error => this.showNotice("Error."))
         },
         /**  process the search form by sending the query to NodeJS */
-        search(){ 
+        search() {
             this.notice = null; // reset notices/messages 
-            axios.get('/search', {params: {filter: this.searchFilter}} )
-                .then(response => {
-                    if (Array.isArray(response.data)) return this.results = response.data
-                    this.showNotice(response.data)
-                 })
-                .catch(error =>  this.showNotice("No Results.") );
+            this.results = [];
+            let matches = this.pokemon.filter(pokemon => pokemon.name.includes(this.searchFilter))
+            // console.log(matches);
+            // this.results = matches;
+
+            for (match of matches.slice(0, 25)) {
+                // console.log(match);
+
+                axios.get(match.url)
+                    .then(resp => {
+
+                        // console.log(resp.data);
+                        this.results.push(resp.data)
+
+                        // this.moves = resp.data.move;
+                        // console.log(this.moves);
+                    })
+            }
+
+
+
+            // selected
+
+
+            // axios.get('/search', {params: {filter: this.searchFilter}} )
+            //     .then(response => {
+            //         if (Array.isArray(response.data)) return this.results = response.data
+            //         this.showNotice(response.data)
+            //      })
+            //     .catch(error =>  this.showNotice("No Results.") );
+
         },
         /** Skip a result (e.g. in case of 403 errors on images ) */
-        skip(crop){
-            this.results =  this.results.filter(x => x.id !== crop.id);  
-        },  
-        
-      
+        skip(pokemon) {
+            this.results = this.results.filter(x => x.name !== pokemon.name);
+        },
+        // searchMoves(){
+        //     this.notice = null; // reset notices/messages 
+        //     // this.results = [];
+        //     // let matches = this.pokemon.filter(pokemon => pokemon.name.includes(this.searchFilter))
+        //     // console.log(matches);
+        //     // let matches = this.pokemon.filter(pokemon => pokemon.name.includes(pokemon.name));
+        //     console.log(this.pokemon);
+
+        //     axios.get(pokemon.url)
+        //     .then( resp =>  {
+
+        //         // console.log(resp.data);
+        //         // this.results.push(resp.data)
+        //         console.log("working");
+        //         // this.moves = resp.data.move;
+        //         // console.log(this.moves);
+        //     })
+        // },
+
+
         /** Check if a given search result is owned
-         * (i.e. it already exists in the farmer's plot). */
-        isOwned(crop){
-            if ( this.plot.crops.filter(x => x.id == crop.id).length > 0 ) return true;
+         * (i.e. it already exists in the team's plot). */
+        isOwned(pokemon) {
+            if (this.team.pokemons.filter(x => x.name == pokemon.name).length > 0) return true;
             return false;
         },
-        /** add a given crop to the farmer's plot.  */
-        add(crop){
-            if ( this.isOwned(crop)) return
-            this.plot.crops.push(crop);
+        /** add a given pokemon to the team's team.  */
+        add(pokemon) {
+
+            if (this.team.pokemons.length < 6) {
+                if (this.isOwned(pokemon)) return
+                this.team.pokemons.push(pokemon);
+                this.save()
+            } else {
+                this.notice = "team full, you can have a max of 6 team members"
+            }
+
+        },
+        changeMove(pokemonMove, pokemonIndex, moveNumber) {
+            // pokemonMove = the selected move from drop down, pokemonIndex = the pokemon, moveNumber = which text box
+
+            // let move1 = this.team.pokemon.move1;
+
+
+            // get the index of the object
+            let pokemonIndexNumber = this.team.pokemons.indexOf(pokemonIndex);
+
+            // assign move variable
+            let move1 = this.team.pokemons[pokemonIndexNumber].move1;
+            let move2 = this.team.pokemons[pokemonIndexNumber].move2;
+            let move3 = this.team.pokemons[pokemonIndexNumber].move3;
+            let move4 = this.team.pokemons[pokemonIndexNumber].move4;
+
+            
+
+
+            // changed the move based on the text box
+            switch (moveNumber) {
+                case 1:
+                    // if the selected move is already selected
+                    if (move2 == pokemonMove || move3 == pokemonMove || move4 == pokemonMove) {
+                        this.showNotice("Error, you can't select the same move twice")
+                    } else {
+                        // change/add the move1 object in the pokemon object
+                        this.team.pokemons[pokemonIndexNumber].move1 = pokemonMove;
+
+                        //only save if move switch is successful
+                        this.save()
+                        // document.getElementById('selectMove').value = none;
+                    }
+                    break;
+                case 2:
+                    if (move1 == pokemonMove || move3 == pokemonMove || move4 == pokemonMove) {
+                        this.showNotice("Error, you can't select the same move twice")
+                    } else {
+                        this.team.pokemons[pokemonIndexNumber].move2 = pokemonMove;
+                        this.save()
+                    }
+                    break;
+                case 3:
+                    if (move2 == pokemonMove || move1 == pokemonMove || move4 == pokemonMove) {
+                        this.showNotice("Error, you can't select the same move twice")
+                    } else {
+                        this.team.pokemons[pokemonIndexNumber].move3 = pokemonMove;
+                        this.save()
+                    }
+                    break;
+                case 4:
+                    if (move2 == pokemonMove || move3 == pokemonMove || move1 == pokemonMove) {
+                        this.showNotice("Error, you can't select the same move twice")
+                    } else {
+                        this.team.pokemons[pokemonIndexNumber].move4 = pokemonMove;
+                        this.save()
+                    }
+                    break;
+            }
+
+
+            // console.log(pokemonMove);
+            // console.log(this.team.pokemons[0].move1);
+
+
+
+        },
+        /** remove a given pokemon from the team's team.  */
+        remove(pokemon) {
+            this.team.pokemons = this.team.pokemons.filter(x => x.name !== pokemon.name);
             this.save()
         },
-        /** remove a given crop from the farmer's plot.  */
-        remove(crop){
-            this.plot.crops = this.plot.crops.filter(x => x.id !== crop.id); 
-            this.save()
+        save() {
+            axios.post('/team', this.team)
+                .then(() => this.showNotice("Data Saved."))
+                .catch(() => this.showNotice("Unable to Save Data."));
         },
-        save(){
-            axios.post(  '/plot',  this.plot )
-                .then(() => this.showNotice("Data Saved.") )
-                .catch(() =>  this.showNotice("Unable to Save Data.") );
-        },
-        showNotice(text){
+        showNotice(text) {
             this.notice = text;
             setTimeout(() => this.notice = false, 2000);
         },
-        /** Hide the search to reveal the farmer's plot (current list of owned crops) */
-        clearSearch(){
+        /** Hide the search to reveal the team's team (current list of owned pokemons) */
+        clearSearch() {
             this.searchFilter = null;
             this.results = null;
             this.notice = null;
         }
+    },
+    mounted() {
+        /* Using axios to fetch data. 
+        You could also use Fetch API if you prefer*/
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20000')
+            .then(response => {
+                console.log(response);
+                this.pokemon = response.data.results
+            })
+            .catch(error => console.log(error));
     }
-
 }).mount('#app')
